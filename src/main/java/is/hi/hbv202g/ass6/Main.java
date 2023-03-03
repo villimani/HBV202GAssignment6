f42b739f1925eb9f4920d31bb8520413c0dee89c
villimani <vilmundurmani@gmail.com>
2023-03-03 08:42:47 +0000

    Main buið að refactora svo að getname virki

--- src/main/java/is/hi/hbv202g/ass6/Main.java
+++ src/main/java/is/hi/hbv202g/ass6/Main.java
@@ -3,8 +3,8 @@ package is.hi.hbv202g.ass6;
 public class Main {
     public static void main(String[] args) {
         Car car=new Car("Beetle");
-        CarOwner carOwner =new CarOwner("John Doe", car);
+        CarOwner carOwner =new CarOwner("John Doe");
         System.out.println("Name of car owner: "+ carOwner.getName());
-        System.out.println("Name of owned car: "+ carOwner.getNameOfCar());
+        System.out.println("Name of owned car: "+ car.getName());
     }
 }
